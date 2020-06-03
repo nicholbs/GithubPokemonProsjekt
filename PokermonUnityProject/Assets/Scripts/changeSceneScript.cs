446751be648ffd71e4e0b5be2384a13e7802d1a2
nicholbs <nicholbs@hotmail.com>
2020-06-04 01:26:13 +0200

    UI
    Lagd UI første utkast med fungerende play button.
    UI:
    Bakgrunnsbilde (ninjaUniversitet) i tulleBilder folder.
    Play, Load, Save buttons.
    Music checkOff + slider
    
    play knappen har scriptet "changeToScene" som tar string navn på scene som det skal byttes til.

---
+++ PokermonUnityProject/Assets/Scripts/changeSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeSceneScript : MonoBehaviour
+{
+
+    /**********************************************************************//**
+    * Funksjon som forandrer "Scene" i Unity.
+    *
+    * Funksjonen tar string av navnet på scene det skal byttes til.
+    * @param string sceneToChangeTo - navn på scene som skal byttes til
+    **************************************************************************/
+    public void changeToScene(string sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+        
+    }
+}
