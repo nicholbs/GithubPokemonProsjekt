a8b5a40f9b5a189611c08f678dc757fe18d31798
nicholbs <nicholbs@hotmail.com>
2020-06-07 19:48:26 +0200

    Pause game
    Laget script for Pause game, men nå kan ikke spiller bevege seg når spillet startes. Jeg tror det har med at spillet er satt med Time.timescale = 0 kanskje eller at du må disable det jævla pause panelet noe slikt: PauseMenu.SetActive (false);

---
+++ PokermonUnityProject/Assets/Scripts/Script_PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class Script_PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    void Start()
+    {
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+    }
+
+
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+//Kan være bedre å ikke pause spillet når PauseMenu kommer opp, men istedenfor disable TileBasedMovementScript slik at input kan bli registrert på meny istedenfor
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+
+    public void LoadMeny(Object sceneToLoad)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneToLoad.name);
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
