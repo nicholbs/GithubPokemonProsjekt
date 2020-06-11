735d63dd20d2e0c3691f6b223df88bf13c6f2ab7
nicholbs <nicholbs@hotmail.com>
2020-06-11 17:52:22 +0200

    Update bossshadow.cs

--- PokermonUnityProject/Assets/bossshadow.cs
+++ PokermonUnityProject/Assets/bossshadow.cs
@@ -8,20 +8,12 @@ using UnityEngine.UI;
 public class Bossshadow : MonoBehaviour
 {
 
-
-
     public Image RGBTilBoss;
-  
-    // Start is called before the first frame update
+ 
     void Start()
     {
         RGBTilBoss.canvasRenderer.SetAlpha(0.0f);
-        //var tempColor = RGBTilBoss.GetComponent<Image>().color;
-        //tempColor.r = 0f;
-        //tempColor.g = 0f;
-        //tempColor.b = 0f;
-
-        //RGBTilBoss.color = tempColor;
+    
         StartCoroutine(fadein());
 
     }
@@ -32,27 +24,5 @@ public class Bossshadow : MonoBehaviour
         yield return new WaitForSeconds(2f);
         RGBTilBoss.CrossFadeAlpha(1, 2, false);
     }
-
-    //void Update()
-    //{
-
-    //    var tempColor = RGBTilBoss.GetComponent<Image>().color;
-    //    if (tempColor.r <= 255)
-    //    {
-    //        tempColor.r += 2.55f;
-    //        tempColor.g += 2.55f;
-    //        tempColor.b += 2.55f;
-    //        RGBTilBoss.color = tempColor;
-    //    }
-    //    else
-    //    {
-    //        tempColor.r += 2.55f;
-    //        tempColor.g += 2.55f;
-    //        tempColor.b += 2.55f;
-    //        RGBTilBoss.color = tempColor;
-    //    }
-
-    //}
-
 
 }
