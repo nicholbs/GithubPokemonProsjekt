970dc4d73c51a3eeb7b2aaa8c5600e8bc59e16bb
Deedern <66326866+Deedern@users.noreply.github.com>
2020-06-11 17:36:14 +0200

    Lagt til player i Boss_scene
    Player "ease out" inn på canvas.

---
+++ PokermonUnityProject/Assets/playermovesin.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playermovesin : MonoBehaviour
+{
+    float lerpTime = 1f;
+    float currentLerpTime;
+
+    float moveDistance = 600f;
+
+    Vector3 startPos;
+    Vector3 endPos;
+
+    protected void Start()
+    {
+        startPos = transform.position;
+        endPos = transform.position + transform.right * moveDistance;
+    }
+
+    protected void Update()
+    {
+        StartCoroutine(MyFunction());
+    }
+    
+    IEnumerator MyFunction()
+    {
+        yield return new WaitForSeconds(2f);
+
+        //reset when we press spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentLerpTime = 0f;
+        }
+
+        //increment timer once per frame
+        currentLerpTime += Time.deltaTime;
+        if (currentLerpTime > lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+
+        //lerp!
+        //float perc = currentLerpTime / lerpTime;
+        float perc = currentLerpTime / lerpTime;
+        perc = Mathf.Sin(perc * Mathf.PI * 0.5f);
+        transform.position = Vector3.Lerp(startPos, endPos, perc);
+    }
+}
+
+//EASE OUT
+//float t = currentLerpTime / lerpTime;
+//t = Mathf.Sin(t* Mathf.PI* 0.5f);
